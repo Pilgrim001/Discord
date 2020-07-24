17cf239395e4b868ff8df70a042ce6c1119c8be5
Pilgrim001 <saeedbello12@gmail.com>
2020-07-24 10:48:06 +0100

    Fixed MongoDB

--- utils/dbconnection.js
+++ utils/dbconnection.js
@@ -1,5 +1,5 @@
 const  mongoose  = require("mongoose");
 mongoose.Promise  = require("bluebird");
-const { DB } = require('./config')
+const { DB } = require('../config/index')
 const  connect  =  mongoose.connect(DB, { useNewUrlParser: true  });
 module.exports  =  connect;
